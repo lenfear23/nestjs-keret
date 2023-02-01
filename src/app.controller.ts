07331e5e7dd241f8daf35175ed69f9f806aaf043
lenfear23 <papp.david@petrik.hu>
2023-02-01 15:27:48 +0100

    x

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,17 @@
-import { Body, Controller, Get, Post, Render } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Get,
+  Post,
+  Render,
+} from '@nestjs/common';
 import { DataSource } from 'typeorm';
 import { AppService } from './app.service';
+import registerDto from './register.dto';
+import RegisterDto from './register.dto';
+import User from './user.entity';
+import * as bcrypt from 'bcrypt';
 
 @Controller()
 export class AppController {
@@ -13,5 +24,29 @@ export class AppController {
   @Render('index')
   index() {
     return { message: 'Welcome to the homepage' };
+  }
+  @Post('/register')
+  register(@Body() registerdto: RegisterDto) {
+    if (
+      !registerdto.email ||
+      !registerdto.password ||
+      registerdto.passwordAgain
+    ) {
+      throw new BadRequestException('All fields are required');
+    }
+    if (registerdto.email.includes('@')) {
+      throw new BadRequestException('Email must contain a @ charater');
+    }
+    if (registerdto.password !== registerdto.passwordAgain) {
+      throw new BadRequestException('Two password not the same');
+    }
+    if (registerdto.passwordAgain.length < 8) {
+      throw new BadRequestException('Min. 8 charater0');
+    }
+    const userRepo = this.dataSource.getRepository(User);
+    const user = new User();
+    user.email = registerdto.email;
+    user.password = bcrypt.hash(registerdto.password, 15);
+    userRepo.save(user);
   }
 }
---
+++ src/user.entity.ts
@@ -0,0 +1,15 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export default class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+  @Column()
+  email: string;
+  @Column()
+  password: string;
+  @Column()
+  profilePictureUrl: string;
+  @Column()
+  registrationDone: Date;
+}
